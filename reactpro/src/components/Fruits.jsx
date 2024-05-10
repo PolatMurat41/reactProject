1632e1305ba981aaae5a70a4608c39229ce428fb
PolatMurat41 <sea.ase112@gmail.com>
2024-05-10 03:39:09 +0300

    14

--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -1,13 +1,23 @@
 export default function Fruits() {
-  const fruits = ["apple", "mango", "banane", "orange", "Pineapple"];
+  const fruits = [
+    { name: "Apple", price: 10, emoji: "😎" },
+    { name: "Mango", price: 7, emoji: "😎" },
+    { name: "Banana", price: 2, emoji: "😎" },
+    { name: "Orange", price: 5, emoji: "😎" },
+    { name: "Pinneapple", price: 8, emoji: "😎" },
+  ];
 
   return (
     <div>
-    <ul>
-      {fruits.map((fruit) => (
-        <li key={fruit}>{fruit}</li>
-      ))}
-    </ul>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.name}>
+            {fruit.emoji}
+            {fruit.name}${fruit.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+//in order to js in jsx {}
