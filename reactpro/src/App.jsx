cc6e9dc2da4091c9e417f2465777798008303990
PolatMurat41 <sea.ase112@gmail.com>
2024-05-11 03:22:57 +0300

    finished

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -3,13 +3,15 @@ import Hello from "./components/Hello";
 import ConditionalComponent from "./components/ConditionalComponent";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
+import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
       <Fruits />
       <ConditionalComponent />
       <Message />
-      <Counter/>
+      <Counter />
+      <Form/>
     </div>
   );
 }
